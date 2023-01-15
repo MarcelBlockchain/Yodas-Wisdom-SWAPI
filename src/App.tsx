2b7dcda1afb98943b6f7a6a3e97dbad5557050b7
Marcel Morales <marcel.morales.dev@gmail.com>
2023-01-15 22:57:28 +0100

    Add scattered stars in background

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+  // Star generating code from: https://codepen.io/christopherkade/pen/rJVPjz
+  // Sets the number of stars we wish to display
+  const numStars = 100
+
+  // For every star we want to display
+  for (let i = 0; i < numStars; i++) {
+    const star = document.createElement('div')
+    star.className = 'star'
+    const xy = getRandomPosition()
+    star.style.top = xy[0].toString() + 'px'
+    star.style.left = xy[1].toString() + 'px'
+    document.body.append(star)
+  }
+
+  // Gets random x, y values based on the size of the container
+  function getRandomPosition (): number[] {
+    const y = window.innerWidth
+    const x = window.innerHeight
+    const randomX = Math.floor(Math.random() * x)
+    const randomY = Math.floor(Math.random() * y)
+    return [randomX, randomY]
+  }
 
 function App() {
   return (
